517059f7871829a3955b3f290114021d0ee5baf5
temirovv <temirovv21@gmail.com>
2023-07-20 11:59:20 +0500

    settings file updated

--- instagram/settings.py
+++ instagram/settings.py
@@ -26,6 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # packages
+    'rest_framework',
+
+    # installed apps
+    
 ]
 
 MIDDLEWARE = [
