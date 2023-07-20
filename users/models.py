f81fff235d4dddc82d81e179ffe116559f6f4a7b
temirovv <temirovv21@gmail.com>
2023-07-20 12:43:00 +0500

    Users model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+ORDINARY_USER, MANAGER, ADMIN = ("ordinary_user", "manager", "admin")
+VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
+NEW, CODE_VERIFIED, DONE, PHOTO_STEP = ("new", "code_verified", "done", "photo_step")
+
+class User(AbstractUser):
+    USER_ROLES = (
+        (ORDINARY_USER, ORDINARY_USER),
+        (MANAGER, MANAGER),
+        (ADMIN, ADMIN)
+    )
+
+    AUTH_TYPE_CHOICES = (
+        (VIA_PHONE, VIA_PHONE),
+        (VIA_EMAIL, VIA_EMAIL)
+    )
+
+    AUTH_STATUS = (
+        (NEW, NEW),
+        (CODE_VERIFIED, CODE_VERIFIED),
+        (DONE, DONE),
+        (PHOTO_STEP, PHOTO_STEP)
+    )
+
+    user_roles = models.CharField(max_length=35, choices=USER_ROLES, default=ORDINARY_USER)
+    AUTH_TYPE = models.CharField(max_length=35, choices=AUTH_TYPE_CHOICES)
+    AUTH_STATUS = models.CharField(max_length=35, choices=AUTH_STATUS, default=NEW)
+    email = models.EmailField(null=True, blank=True, unique=True)
+    phone_number = models.CharField(max_length=35, null=True, blank=True, unique=True)
+    photo = models.ImageField(upload_to="user_photos/", null=True, blank=True)
