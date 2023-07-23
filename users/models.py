98436ae2f869f1694b058cb692d07a51b057bbd1
temirovv <temirovv21@gmail.com>
2023-07-23 11:49:33 +0500

    User model updated: new methods added

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 import random
+import uuid
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from shared.models import BaseModel
 
@@ -59,8 +61,45 @@ class User(AbstractUser, BaseModel):
         )
         return code
 
-
+    def check_username(self):
+        if not self.username:
+            temp_username = f"instagram-{uuid.uuid4.__str__().split('-')[-1]}"
+            while User.objects.filter(username=temp_username):
+                temp_username = f"{temp_username}{random.randint(0,9)}"
+            self.username = temp_username
     
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+    
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4.__str__().split('-')[-1]}"
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith("pbkdf2_sha256"):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "access": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        }
+
+    def clean(self):
+        self.check_email()
+        self.check_username()
+        self.check_pass()
+        self.hashing_password()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.clean()
+        super(User, self).save(*args, **kwargs)
+
 
 EMAIL_EXPIRE = 5
 PHONE_EXPIRE = 2
