95a5322205c21a140205ed79193e8f8157f301d2
temirovv <temirovv21@gmail.com>
2023-09-17 13:04:46 +0500

    models registered

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, UserConfirmation
 
-# Register your models here.
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'phone_number']
+
+class UserConfimationModelAdmin(admin.ModelAdmin):
+    list_display = ['code', 'verify_type', 'user', 'expiration_time', 'is_confirmed']
+
+admin.site.register(User, UserModelAdmin)
+admin.site.register(UserConfirmation, UserConfimationModelAdmin)
