2340c69109255c23f9bdfac537b57e5c343d7421
temirovv <temirovv21@gmail.com>
2023-09-17 13:01:20 +0500

    created view for signing up

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+
+from .serializers import SignUpSerializer
+from .models import User
+
+
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permisson_classes = (permissions.AllowAny, )
+    serializer_class = SignUpSerializer
