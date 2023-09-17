410f28e913fc707f82091c0ea196cd5507fa8d66
temirovv <temirovv21@gmail.com>
2023-09-17 13:00:27 +0500

    users model updated

--- users/models.py
+++ users/models.py
@@ -34,8 +34,8 @@ class User(AbstractUser, BaseModel):
     )
 
     user_roles = models.CharField(max_length=35, choices=USER_ROLES, default=ORDINARY_USER)
-    AUTH_TYPE = models.CharField(max_length=35, choices=AUTH_TYPE_CHOICES)
-    AUTH_STATUS = models.CharField(max_length=35, choices=AUTH_STATUS, default=NEW)
+    auth_type = models.CharField(max_length=35, choices=AUTH_TYPE_CHOICES)
+    auth_status = models.CharField(max_length=35, choices=AUTH_STATUS, default=NEW)
     email = models.EmailField(null=True, blank=True, unique=True)
     phone_number = models.CharField(max_length=35, null=True, blank=True, unique=True)
     photo = models.ImageField(
@@ -52,8 +52,15 @@ class User(AbstractUser, BaseModel):
     def __str__(self) -> str:
         return self.username
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     def create_verify_code(self, verify_type):
-        code = ''.join([str(random.randint(0, 100)) % 10 for _ in range(4) ])
+        '''
+            user confirmation uchun tasdiqlash kodini jo'natish
+        '''
+        code = ''.join([str(random.randint(0, 100) % 10) for _ in range(4) ])
         UserConfirmation.objects.create(
             user_id=self.id,
             verify_type=verify_type,
@@ -62,43 +69,60 @@ class User(AbstractUser, BaseModel):
         return code
 
     def check_username(self):
+        '''
+        Username ni tekshiramiz agar mavjud bo'lmasa
+        vaqtinchalik username yaratib qo'yamiz 
+        '''
         if not self.username:
-            temp_username = f"instagram-{uuid.uuid4.__str__().split('-')[-1]}"
+            temp_username = f"instagram-{uuid.uuid4().__str__().split('-')[-1]}"
+            print(f"{temp_username=}*************************************************")
             while User.objects.filter(username=temp_username):
                 temp_username = f"{temp_username}{random.randint(0,9)}"
             self.username = temp_username
     
     def check_email(self):
+        '''
+            email borligi yoki yo'qligini tekshirish
+        '''
         if self.email:
             normalize_email = self.email.lower()
             self.email = normalize_email
     
     def check_pass(self):
+        '''
+            Parol bo'lmasa o'rniga vaqtinchalik parol yaratib qo'yamiz
+        '''
         if not self.password:
             temp_password = f"password-{uuid.uuid4.__str__().split('-')[-1]}"
             self.password = temp_password
 
     def hashing_password(self):
+        '''
+            Agar parol heshlanmagan bo'lgan bo'lsa
+            uni heshlab qo'yamiz
+        '''
         if not self.password.startswith("pbkdf2_sha256"):
             self.set_password(self.password)
 
     def token(self):
+        ''' Tokenni yangilash uchun funksiya '''
         refresh = RefreshToken.for_user(self)
         return {
             "access": str(refresh.access_token),
             "refresh_token": str(refresh)
         }
 
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(User, self).save(*args, **kwargs)
+
     def clean(self):
+        ''' bu yerda yuqorida yozilgan barcha funksiyalarni chaqiramiz '''
         self.check_email()
         self.check_username()
         self.check_pass()
         self.hashing_password()
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.clean()
-        super(User, self).save(*args, **kwargs)
+        print('*****', self.username)
 
 
 EMAIL_EXPIRE = 5
@@ -111,8 +135,9 @@ class UserConfirmation(BaseModel):
     code = models.CharField(max_length=4)
     verify_type = models.CharField(max_length=31, choices=TYPE_CHOICES)
     user = models.ForeignKey("users.User", models.CASCADE, related_name='verify_codes')
-    expiration_time = models.DateTimeField(null=True)
-    is_confirmed = models.BooleanField(default=False)
+    # users appdagi User Modelini ko'rsatish
+    expiration_time = models.DateTimeField(null=True) # tugash vaqti
+    is_confirmed = models.BooleanField(default=False) # tasdiqlandimi yoki yo'qmi
 
     def __str__(self) -> str:
         return str(self.user.__str__())
